5bc8f460586a4acfd5e7a20f9f0247615b697a51
Jonah <73760377+jonerrr@users.noreply.github.com>
2022-11-01 10:15:45 -0400

    rewrite stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 use opencv::{core, highgui, imgproc, objdetect, prelude::*, types, videoio, Result};
-use std::{cmp::min_by, collections::VecDeque};
-
-// #[derive(Debug)]
-// struct Points {
-//     x: Vec<i32>,
-//     y: Vec<i32>,
-//     center: f32,
-// }
-
+use std::collections::VecDeque;
 #[derive(Debug)]
 struct Points {
     faces: VecDeque<core::Point_<i32>>,
@@ -17,28 +9,10 @@ struct Points {
 impl Points {
     fn new(centers: (i32, i32)) -> Self {
         Points {
-            faces: VecDeque::from([
-                core::Point_ {
-                    x: (centers.0 + centers.1 / 2),
-                    y: 300,
-                },
-                // core::Point_ {
-                //     x: (centers.0 + centers.1 / 2),
-                //     y: 300,
-                // },
-                // core::Point_ {
-                //     x: (centers.0 + centers.1 / 2),
-                //     y: 300,
-                // },
-                // core::Point_ {
-                //     x: (centers.0 + centers.1 / 2),
-                //     y: 300,
-                // },
-                // core::Point_ {
-                //     x: (centers.0 + centers.1 / 2),
-                //     y: 300,
-                // },
-            ]),
+            faces: VecDeque::from([core::Point_ {
+                x: (centers.0 + centers.1 / 2),
+                y: 300,
+            }]),
             centers,
         }
     }
@@ -47,55 +21,54 @@ impl Points {
         &mut self,
         faces: core::Vector<core::Rect_<i32>>,
     ) -> (core::Point_<i32>, core::Point_<i32>) {
-        faces.iter().for_each(|f| {
+        let last_face = self.faces.back().unwrap().x;
+        let closest = faces.iter().reduce(|accum, f| {
+            let dx = (accum.x + accum.width / 2 - last_face).abs();
+            if dx <= (f.x + f.width / 2 - last_face).abs() {
+                accum
+            } else {
+                f
+            }
+        });
+
+        if let Some(f) = closest {
             self.faces.push_back(core::Point_ {
                 x: f.x + f.width / 2,
                 y: f.y + f.height / 2,
-            })
-        });
+            });
+        }
 
-        if self.faces.len() > 5 {
+        if self.faces.len() > 3 {
             self.faces.pop_front();
         }
 
-        (*self.faces.back().unwrap(), *self.faces.back().unwrap())
+        let mut tx = 0;
+        let mut ty = 0;
+
+        for f in self.faces.iter() {
+            tx += f.x;
+            ty += f.y;
+        }
+        tx = tx / self.faces.len() as i32;
+        ty = ty / self.faces.len() as i32;
+
+        let dx = (tx - 700) as f32;
+        let dy = (ty - 300) as f32;
+        let px = 50.0 * (dy.atan2(dx)).cos();
+        let py = 50.0 * (dy.atan2(dx)).sin();
+
+        (
+            core::Point_ {
+                x: px as i32 + self.centers.0,
+                y: py as i32 + 300,
+            },
+            core::Point_ {
+                x: px as i32 + self.centers.1,
+                y: py as i32 + 300,
+            },
+        )
     }
 }
-
-// impl Points {
-//     fn new(center: f32) -> Self {
-//         Points {
-//             x: vec![400],
-//             y: vec![300],
-//             center,
-//             // last_face: None,
-//         }
-//     }
-
-//     fn put_point(&mut self, x: f32, y: f32) {
-//         let dx = x - 700.0;
-//         let dy = y - 300.0;
-
-//         let px = 50.0 * (dy.atan2(dx)).cos();
-//         let py = 50.0 * (dy.atan2(dx)).sin();
-
-//         println!("x: {}\ny: {}", px, py);
-
-//         self.x.push((px + &self.center) as i32);
-//         self.y.push(py as i32 + 300);
-//         if self.x.len() > 2 {
-//             self.x.remove(0);
-//             self.y.remove(0);
-//         }
-//     }
-
-//     fn get_point(&self) -> core::Point_<i32> {
-//         core::Point_ {
-//             x: &self.x.iter().sum() / *&self.x.len() as i32,
-//             y: &self.y.iter().sum() / *&self.y.len() as i32,
-//         }
-//     }
-// }
 
 fn main() -> Result<()> {
     let window = "Stalker";
@@ -111,10 +84,7 @@ fn main() -> Result<()> {
     //     "/usr/local/Cellar/opencv/4.6.0/share/opencv4/haarcascades/haarcascade_profileface.xml",
     // )?;
 
-    // let mut left_eye = Points::new(800.0);
-    // let mut right_eye = Points::new(400.0);
     let mut f = Points::new((400, 800));
-
     let mut img = Mat::default();
 
     loop {
@@ -141,19 +111,19 @@ fn main() -> Result<()> {
         //     core::Size::new(0, 0),
         // )?;
 
-        // imgproc::rectangle(
-        //     &mut img,
-        //     core::Rect {
-        //         x: 0,
-        //         y: 0,
-        //         width: 5000,
-        //         height: 5000,
-        //     },
-        //     core::Scalar::new(255f64, 255f64, 255f64, 255f64),
-        //     -1,
-        //     8,
-        //     0,
-        // )?;
+        imgproc::rectangle(
+            &mut img,
+            core::Rect {
+                x: 0,
+                y: 0,
+                width: 2000,
+                height: 2000,
+            },
+            core::Scalar::new(255f64, 255f64, 255f64, 255f64),
+            -1,
+            8,
+            0,
+        )?;
 
         // Eyebrows
         imgproc::ellipse(
@@ -219,52 +189,11 @@ fn main() -> Result<()> {
             0,
         )?;
 
-        // let target = faces.get(0);
-
-        // let face = faces.iter().map(|p| {
-        //     closest_face = min_by(
-        //         closest_face,
-        //         core::Point_ {
-        //             x: *left_eye.x.last().unwrap(),
-        //             y: *left_eye.y.last().unwrap(),
-        //         },
-        //         |a, b| {
-
-        //         },
-        //     );
-        // });
-        // match target {
-        //     Ok(t) => {
-        //         let mut cx = t.x + t.width / 2;
-        //         let mut cy = t.y + t.height / 2;
-
-        //         if faces.len() > 1 {
-        //             for f in faces.iter() {
-        //                 let dx = (*left_eye.x.last().unwrap() as i32 - (f.x + f.width / 2)).abs();
-        //                 let dy = (*left_eye.y.last().unwrap() as i32 - (f.y + f.height / 2)).abs();
-        //                 if dx < (cx - (f.x + f.width / 2)).abs()
-        //                     || dx < (cy - (f.y + f.height / 2)).abs()
-        //                 {
-        //                     cx = dx;
-        //                     cy = dy;
-        //                 }
-        //             }
-        //         }
-
-        //         left_eye.put_point(cx as f32, cy as f32);
-        //         right_eye.put_point(cx as f32, cy as f32);
-        //     }
-        //     Err(_) => {
-        //         left_eye.put_point(400.0, 300.0);
-        //         right_eye.put_point(900.0, 300.0);
-        //     }
-        // }
-
-        f.get_point(faces);
+        let points = f.get_point(faces);
 
         imgproc::circle(
             &mut img,
-            *f.faces.back().unwrap(),
+            points.0,
             30,
             core::Scalar::new(200f64, 50f64, 0f64, 0f64),
             -1,
@@ -273,7 +202,7 @@ fn main() -> Result<()> {
         )?;
         imgproc::circle(
             &mut img,
-            *f.faces.back().unwrap(),
+            points.1,
             30,
             core::Scalar::new(200f64, 50f64, 0f64, 0f64),
             -1,
@@ -285,7 +214,7 @@ fn main() -> Result<()> {
         core::flip(&img, &mut flipped, 1)?;
         // imgproc::put_text(
         //     &mut flipped,
-        //     &format!("faces"),
+        //     &format!("faces: {}", faces.len()),
         //     core::Point_ { x: 10, y: 80 },
         //     0,
         //     3.0,
@@ -297,7 +226,7 @@ fn main() -> Result<()> {
 
         highgui::imshow(window, &flipped)?;
 
-        if highgui::wait_key(5)? > 0 {
+        if highgui::wait_key(10)? > 0 {
             break;
         }
     }
